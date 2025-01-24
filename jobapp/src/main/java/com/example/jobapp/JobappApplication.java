7f5b208665437900fb76463c3bf4b5fdf36de10e
rohitjha17092 <rohit.jha@ripplehire.com>
2025-01-24 00:39:39 -0500

    war generated

--- jobapp/src/main/java/com/example/jobapp/JobappApplication.java
+++ jobapp/src/main/java/com/example/jobapp/JobappApplication.java
@@ -2,12 +2,18 @@ package com.example.jobapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobappApplication {
+public class JobappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobappApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JobappApplication.class);
+	}
 }
